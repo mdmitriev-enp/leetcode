9e335c62a1b2476c91147a3778b4297259cc6c63
Maxim Dmitriev <MDmitriev2016@yandex.ru>
2023-12-10 22:48:33 +0400

    Задание 1068

---
+++ SQL/1068 - Query.sql
@@ -0,0 +1,7 @@
+SELECT
+	product.product_name,
+	sales.year,
+	sales.price
+FROM
+	sales
+	LEFT JOIN product ON sales.product_id = product.product_id;
---
+++ SQL/1068 - Schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS Sales (sale_id int, product_id int, year int, quantity int, price int);
+CREATE TABLE IF NOT EXISTS Product (product_id int, product_name varchar(10));
+
+INSERT INTO Sales (sale_id, product_id, year, quantity, price)
+VALUES
+	('1', '100', '2008', '10', '5000'),
+	('2', '100', '2009', '12', '5000'),
+	('7', '200', '2011', '15', '9000');
+
+INSERT INTO Product (product_id, product_name)
+VALUES
+	('100', 'Nokia'),
+	('200', 'Apple'),
+	('300', 'Samsung');
