991b2d414ee9a64a2082619154c4963611ebeb7c
Maxim Dmitriev <MDmitriev2016@yandex.ru>
2023-12-10 22:39:29 +0400

    Задание 1378

---
+++ SQL/1378 - Schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Employees (id int, name varchar(20));
+CREATE TABLE IF NOT EXISTS EmployeeUNI (id int, unique_id int);
+
+INSERT INTO Employees (id, name)
+VALUES
+	('1', 'Alice'),
+	('7', 'Bob'),
+	('11', 'Meir'),
+	('90', 'Winston'),
+	('3', 'Jonathan');
+
+INSERT INTO EmployeeUNI (id, unique_id)
+VALUES
+	('3', '1'),
+	('11', '2'),
+	('90', '3');
