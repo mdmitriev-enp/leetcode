45cc7335a4bd369879fcc53f11b1a17e1287698c
Maxim Dmitriev <MDmitriev2016@yandex.ru>
2023-12-10 21:59:31 +0400

    Задание 595

---
+++ SQL/595/595 - Schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS World (name varchar(255), continent varchar(255), area int, population int, gdp bigint);
+
+INSERT INTO World (name, continent, area, population, gdp) 
+VALUES  ('Afghanistan', 'Asia', '652230', '25500100', '20343000000'),
+		('Albania', 'Europe', '28748', '2831741', '12960000000'),
+		('Algeria', 'Africa', '2381741', '37100000', '188681000000'),
+		('Andorra', 'Europe', '468', '78115', '3712000000'),
+		('Angola', 'Africa', '1246700', '20609294', '100990000000');
