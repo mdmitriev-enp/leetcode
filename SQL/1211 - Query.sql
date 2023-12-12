58b45ddbe5a607f78ae83efe0c8e9eac481c2690
Maxim Dmitriev <MDmitriev2016@yandex.ru>
2023-12-12 16:54:59 +0400

    Задание 1211

--- SQL/1211 - Query.sql
+++ SQL/1211 - Query.sql
@@ -1,40 +1,10 @@
 SELECT
-  queries.query_name,
-  ROUND(
-    (
-      SUM(queries.rating :: numeric / queries.position) / (
-        SELECT
-          NULLIF (COUNT(query_name), 0)
-        FROM
-          queries AS q1
-        WHERE
-          queries.query_name = q1.query_name
-      )
-    ),
-    2
-  ) AS quality,
-  ROUND(
-    (
-      SELECT
-        COUNT(query_name)
-      FROM
-        queries AS q2
-      WHERE
-        queries.query_name = q2.query_name
-        AND q2.rating < 3
-    ) :: numeric * 100 / (
-      SELECT
-        NULLIF(COUNT(query_name), 0)
-      FROM
-        queries AS q1
-      WHERE
-        queries.query_name = q1.query_name
-    ),
-    2
-  ) AS poor_query_percentage
+	queries.query_name,
+	ROUND (SUM(queries.rating :: numeric / queries.position) / COUNT (*), 2) AS quality,
+	ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100, 2) AS poor_query_percentage
 FROM
-  queries AS queries
+	queries
 GROUP BY
-  query_name
+	query_name
 HAVING
-  query_name IS NOT NULL;
+	query_name IS NOT NULL;
